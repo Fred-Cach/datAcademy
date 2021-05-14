102a219efbe7beb242fa2cab02eedb0bf2be3f9b
Fred-Cach <federicocachero@hotmail.com>
2021-05-14 16:25:57 -0300

    working with Mark, now more better

--- laweamarko.py
+++ laweamarko.py
@@ -1,18 +1,7 @@
 def run():
-    contador = 0
 
-    while contador == 0:
-        dato1 = float(input("¿Cuál es la altura del triángulo? "))
-        if dato1 >= 1:
-            print('exelente, siguiente pregunta')
-            contador += 1
-
-    while contador == 1:
-        dato2 = float(input("¿Cuál es la base del triángulo? "))
-        if dato2 >= 1:
-            print('bien, continuemos')
-            contador += 1
-
+    dato1 = float(input("¿Cuál es la altura del triángulo? "))
+    dato2 = float(input("¿Cuál es la base del triángulo? "))
     area = dato1 * dato2 / 2
     area = str(area)
     print("Acá tenés el área, dejate de romper las bo... " + area)
