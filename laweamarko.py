559b7ad8606ae2c257438403a1f8d850e90dc5a1
Fred-Cach <federicocachero@hotmail.com>
2021-05-14 16:48:15 -0300

    working with Mark

--- laweamarko.py
+++ laweamarko.py
@@ -3,7 +3,7 @@ def run():
     dato1 = float(input("¿Cuál es la altura del triángulo? "))
     dato2 = float(input("¿Cuál es la base del triángulo? "))
     area = dato1 * dato2 / 2
-    area = str(int(area))
+    area = str(area)
     print("Acá tenés el área, dejate de romper las bo... " + area)
 
 
